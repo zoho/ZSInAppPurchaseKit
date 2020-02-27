1507405fa2186bcbc855ccc2a960f748ee8af653
Boobesh <boobesh.s@zohocorp.com>
2020-02-27 12:17:35 +0530

    Adding XCFramework v1.0.0 (4)

---
+++ ZSInAppPurchaseKit.xcframework/ios-i386_x86_64-simulator/ZSInAppPurchaseKit.framework/Headers/ZSKit.h
@@ -0,0 +1,19 @@
+//
+//  ZSKit.h
+//  ZSKit
+//
+//  Created by Boobesh Kumar Suresh on 30/10/19.
+//  Copyright © 2019 Zoho Finance. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZSKit.
+FOUNDATION_EXPORT double ZSKitVersionNumber;
+
+//! Project version string for ZSKit.
+FOUNDATION_EXPORT const unsigned char ZSKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZSKit/PublicHeader.h>
+
+
